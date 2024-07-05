9f4f0600a895ef02b8a4a89d60e25cc5523b260e
kelvinmutugi210 <kelvinmutugi210@gmail.com>
2024-07-05 19:46:12 +0300

    Create json file to store my data, create a reusable skill component and update main component to use the skill component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Skill from './mainComponents/Skill'
+import data from '../assets/data/data.json'
+
 
 function Main() {
   return (
     <main>
-        <h1>Main</h1>
+        <div className='container mx-auto  px-4  sm:px-15 lg:px-32'>
+          <section className='flex flex-col flex-1 border-t-[1.5px] border-t-gray items-center gap-4 py-10 flex-wrap sm:flex-row  justify-between'>
+            {data.skills.map((skill)=> (
+              <Skill
+                key={skill.name}
+                skill={skill.name}
+                experience={skill.experienceYrs}
+              ></Skill>
+            ))}
+            
+          </section>
+        </div>
     </main>
   )
 }
