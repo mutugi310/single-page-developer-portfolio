8c673fb908c897995ada01173b717799a133aa1f
kelvinmutugi210 <kelvinmutugi210@gmail.com>
2024-07-15 21:20:31 +0300

    Update the contact button on the header to scroll smothly to the contact section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
       <div className="container mx-auto   px-4 sm:px-15 lg:px-32">
         <section className="flex flex-col border-t-[1.5px] border-t-gray py-16  w-full gap-4 items-center lg:flex-row lg:justify-between  ">
           <p className="text-2xl text-center">mutugiKelvin</p>
-          <div className="flex justify-evenly sm:gap-4  ">
+          <div className="flex justify-evenly gap-4  ">
             <a
               href="https://www.linkedin.com/in/mutugikelvin"
               target="_blank"
